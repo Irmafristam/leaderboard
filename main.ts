fb9066de501f9117ac48d4c9bbb7a205364b0598
Irma Fristam <144234580+Irmafristam@users.noreply.github.com>
2023-09-07 16:38:20 +0200

    Initiala filer för MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.B, function () {
+    let emoji = 0
+    if (emoji == 0) {
+        basic.showIcon(IconNames.SmallHeart)
+    } else if (emoji == 1) {
+        basic.showIcon(IconNames.Heart)
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+	
+})
